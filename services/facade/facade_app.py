f64922f7b3adbe9e292e8f75fe01b3ac76940e26
Alex Mytnyk <alex.mytnyk@ukr.net>
2024-05-14 05:22:47 +0300

    hazelcast

--- services/facade/facade_app.py
+++ services/facade/facade_app.py
@@ -1,5 +1,6 @@
 import random
 
+import hazelcast
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uuid
@@ -13,15 +14,27 @@ class Message(BaseModel):
 
 
 logging_service_urls = ["http://localhost:8011/log", "http://localhost:8012/log", "http://localhost:8013/log"]
-messages_service_url = "http://localhost:8004/message"
+messages_service_urls = ["http://localhost:8021/message", "http://localhost:8022/message"]
+
+client = hazelcast.HazelcastClient(
+    cluster_name="cluster",
+    cluster_members=["127.0.0.1:5704"]
+)
+
+message_queue = client.get_queue("message_queue").blocking()
 
 
 def random_logging_service_url():
     return random.choice(logging_service_urls)
 
 
+def random_messages_service_url():
+    return random.choice(messages_service_urls)
+
+
 @app.post("/facade")
 async def post_message(message: Message):
+    message_queue.offer(message.msg)
     response = requests.post(random_logging_service_url(), json={"id": str(uuid.uuid4()), "msg": message.msg})
     if not response.ok:
         raise HTTPException(status_code=response.status_code, detail=response.reason)
@@ -35,7 +48,7 @@ async def get_messages():
     if not logging_response.ok:
         raise HTTPException(status_code=logging_response.status_code, detail=logging_response.reason)
 
-    messages_response = requests.get(messages_service_url)
+    messages_response = requests.get(random_messages_service_url())
     if not messages_response.ok:
         raise HTTPException(status_code=messages_response.status_code, detail=messages_response.reason)
 
--- services/messages/messages_app.py
+++ services/messages/messages_app.py
@@ -1,8 +1,42 @@
+from queue import Queue
+from threading import Thread
+from typing import Optional
+
+import hazelcast
 from fastapi import FastAPI
 
 app = FastAPI()
 
+client = hazelcast.HazelcastClient(
+    cluster_name="cluster",
+    cluster_members=["127.0.0.1:5704"]
+)
+
+message_queue = client.get_queue("message_queue").blocking()
+messages = []
+thread: Optional[Thread] = None
+
+
+def run_consumer():
+    while True:
+        message = message_queue.take()
+        print(f"Received message: {message}")
+        messages.append(message)
+
+
+@app.on_event("startup")
+async def startup():
+    global thread
+    thread = Thread(target=run_consumer, daemon=True)
+    thread.start()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    global thread
+    thread.join(0)
+
 
 @app.get("/message")
 async def get_message():
-    return "not implemented yet"
+    return messages
